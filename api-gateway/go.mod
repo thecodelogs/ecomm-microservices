fafb9e1a7be6c747149f29097d5562065f22676f
Manoj Singh Negi <www.msnegi@gmail.com>
2026-05-07 16:31:49 +0530

    product listing fixes

--- api-gateway/go.mod
+++ api-gateway/go.mod
@@ -4,6 +4,7 @@ go 1.25.1
 
 require (
 	github.com/gin-gonic/gin v1.9.1
+	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
 	github.com/manojnegi/ecomm-microservices/gen/go v0.0.0
 	github.com/o1egl/paseto v1.0.0
--- api-gateway/internal/handler/product_handler.go
+++ api-gateway/internal/handler/product_handler.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	productpb "github.com/manojnegi/ecomm-microservices/gen/go/product/v1"
 )
 
@@ -22,15 +23,34 @@ func (p *ProductHandler) ListProducts(c *gin.Context) {
 	page, _ := strconv.Atoi(c.DefaultQuery("page", "1"))
 	pageSize, _ := strconv.Atoi(c.DefaultQuery("page_size", "20"))
 
+	categoryIDStr := c.Query("category_id")
+
+	var categoryID string
+
+	if categoryIDStr != "" {
+		_, err := uuid.Parse(categoryIDStr)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "invalid category_id",
+			})
+			return
+		}
+
+		categoryID = categoryIDStr
+	}
+
 	ctx, cancel := context.WithTimeout(c.Request.Context(), 5*time.Second)
 	defer cancel()
 
 	resp, err := p.productClient.ListProducts(ctx, &productpb.ListProductsRequest{
-		Page:     int32(page),
-		PageSize: int32(pageSize),
+		CategoryId: categoryID,
+		Page:       int32(page),
+		PageSize:   int32(pageSize),
 	})
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
 		return
 	}
 
